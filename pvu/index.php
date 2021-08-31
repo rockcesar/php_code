22ce0ba7ea79d8ba1b93be2ee84f22d9a4160556
César Cordero Rodríguez <cesar.cordero.r@gmail.com>
2021-08-31 06:06:59 -0400

    Add files via upload

--- pvu/index.php
+++ pvu/index.php
@@ -1,10 +1,18 @@
 <form action="horarios_pvu.php" method="get">
 
+    Code: <a href="https://github.com/rockcesar/php_code/tree/main/pvu">Ver código</a>
+    <br/><br/>
+    Este es un script para generar los horarios de PVU (Plant vs Undead) automáticamente.
+    <br/><br/>
+    Lo que hay que saber es el grupo, alguna hora de inicio de ese grupo, cantidad total de grupos y factor (cada cuanto tiempo cambia de grupo).
+    <br/><br/>
+    Y te genera todos los horarios de todos los grupos.
+    <br/><br/>
     Group Quantity: <input name="group_quantity" value="12"/>
     <br/>
     Group: <input name="group" value="10"/>
     <br/>
-    Hour of group: <input name="hour" value="30-08-2021 18:56:02"/>
+    Hour of group: <input name="hour" value="31-8-2021 08:01:00"/>
     <br/>
     Factor: <input name="factor" value="+20 minute"/>
     <br/>
